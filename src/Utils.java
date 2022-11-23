a5f48ff8bc0342db8653217a7dff966704337787
Blake1412 <blakeryan30@gmail.com>
2022-11-23 23:08:28 +0000

    FileReader utility added by Roger

--- src/Utils.java
+++ src/Utils.java
@@ -1,7 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Utils {
     public static boolean timeOverlap(LocalTime timeOneStart, LocalTime timeOneEnd, LocalTime timeTwoStart, LocalTime timeTwoEnd) {
         return timeOneStart.isBefore(timeTwoEnd) && timeTwoStart.isBefore(timeOneEnd);
+    }
+
+    public static ArrayList<String[]> fileReader(String fileName) {
+        ArrayList<String[]> data = new ArrayList<>();
+        String string;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((string = br.readLine()) != null) {
+                String[] line = string.split(",");
+                data.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("This File does not exist: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Could not read this file: " + fileName);
+        }
+        return data;
     }
 }
